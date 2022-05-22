8bbae68456e0768524ec7dbf6cc411da93caa600
Nina Vinding Blindheim <ninamarstrander@gmail.com>
2022-05-22 14:42:57 +0200

    Add AuthenticationManager and test functions

--- backend/backend.py
+++ backend/backend.py
@@ -1,2 +1,10 @@
+from .authentication import AuthenticationManager
+
+
 def run():
-    ...
+    authentication_manager = AuthenticationManager()
+    input_user = "user1"
+    input_password = "password123"
+
+    authentication_manager.attempt_login(input_user, input_password)
+    authentication_manager.logout(input_user)
