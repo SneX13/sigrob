36aaf1085a55d062ca5ce9ba887622e2b3e58ae9
Nina Vinding Blindheim <ninamarstrander@gmail.com>
2022-06-04 19:59:20 +0200

    Apply all standard Django migrations

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -3,6 +3,6 @@ from django.urls import re_path
 from . import views
 
 urlpatterns = [
-    re_path(r'^users$', views.user_table_methods),
-    re_path(r'^users/([0-9]+)$', views.user_table_methods)
+    re_path(r'^users/$', views.user_table_methods),
+    re_path(r'^users/([0-9]+)/$', views.user_table_methods)
 ]
