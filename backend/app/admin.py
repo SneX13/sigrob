0d395c1351e5b7a70efb1327547aeaecd77e3ada
Nina Vinding Blindheim <ninamarstrander@gmail.com>
2022-06-06 00:56:14 +0200

    Add database tables to admin page

--- backend/app/admin.py
+++ backend/app/admin.py
@@ -1,1 +1,8 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import User, Company, System, Component
+
+admin.site.register(User)
+admin.site.register(Company)
+admin.site.register(System)
+admin.site.register(Component)
--- backend/app/models.py
+++ backend/app/models.py
@@ -2,27 +2,38 @@ from django.db import models
 
 
 class Company(models.Model):
-    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
 
 
 class User(models.Model):
-    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
-    email = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
     password = models.CharField(max_length=255)
     company = models.ForeignKey(Company, models.CASCADE, to_field='name')
     is_admin = models.BooleanField()
 
+    def __str__(self):
+        string = f'{self.company} | {self.name}'
+        if self.is_admin:
+            string += f' (admin)'
+        return string
+
 
 class System(models.Model):
-    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
-    parent = models.IntegerField()
+    parent = models.IntegerField(null=True)
     company = models.ForeignKey(Company, models.CASCADE)
+
+    def __str__(self):
+        return f'{self.company} | {self.name}'
 
 
 class Component(models.Model):
-    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     system = models.OneToOneField(System, models.CASCADE)
+
+    def __str__(self):
+        return f'{self.system} | {self.name} {self.id}'
