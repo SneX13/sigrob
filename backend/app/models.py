f219ea2e34f4f44a6eb2d5816d94d4e9e580b247
Nina Vinding Blindheim <ninamarstrander@gmail.com>
2022-06-08 14:57:44 +0200

    Add custom user manager to authenticate by email

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -10,16 +11,53 @@ class Company(models.Model):
         return self.name
 
 
+class UserManager(BaseUserManager):
+    # A custom user manager to be able to add user with email authentication from:
+
+    # https://koenwoortman.com/python-django-email-as-username/#:~:text=By%20default%
+    # 20these%20Django%20users,migrations%20for%20the%20first%20time.
+    use_in_migration = True
+
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('Users require an email field')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+
+
 class User(AbstractUser):
     username = None
     email = models.EmailField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
     company = models.ForeignKey(Company, models.CASCADE, to_field='name', null=True)
 
+    objects = UserManager()
+
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = [
-        "username", "password", "first_name", "last_name", "is_staff", "company_id"
-    ]
+    REQUIRED_FIELDS = []
+
+    # REQUIRED_FIELDS = [
+    #     "username", "password", "first_name", "last_name", "is_staff", "company_id"
+    # ]
 
     def __str__(self):
         string = f'{self.company} | {self.first_name} {self.last_name}'
