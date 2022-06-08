07f7fe52453e68226c9c3ac18c544aa323ec3c3d
Nina Vinding Blindheim <ninamarstrander@gmail.com>
2022-06-08 14:56:11 +0200

    Add logo (image) functionality to company table

--- backend/app/models.py
+++ backend/app/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Company(models.Model):
     name = models.CharField(max_length=255, unique=True)
+    logo = models.ImageField(upload_to="images/", default=None, null=True)
 
     def __str__(self):
         return self.name
