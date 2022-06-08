27ae19b71683fed0d40c40e827e1833696e0e939
Nina Vinding Blindheim <ninamarstrander@gmail.com>
2022-06-08 13:38:48 +0200

    Add API methods for system table

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('login/', views.LoginAttempt.as_view()),
     path('logout/', views.Logout.as_view()),
-    path('systems/', views.GetSystems.as_view()),
+    path('systems/', views.SystemTable.as_view()),
 ]
--- backend/app/views.py
+++ backend/app/views.py
@@ -55,7 +55,7 @@ class Logout(APIView):
         )
 
 
-class GetSystems(APIView):
+class SystemTable(APIView):
     @staticmethod
     def get(request: http.HttpRequest) -> http.HttpResponse:
         system_data = JSONParser().parse(request)
@@ -79,3 +79,59 @@ class GetSystems(APIView):
             systems = System.objects.filter(company=user.company)
             systems_serializer = SystemSerializer(systems, many=True)
             return http.HttpResponse(systems_serializer.data)
+
+    @staticmethod
+    def post(request: http.HttpRequest) -> http.HttpResponse:
+        system_data = JSONParser().parse(request)
+        system_serializer = SystemSerializer(data=system_data)
+        if system_serializer.is_valid():
+            system_serializer.save()
+            return http.HttpResponse(
+                "Successfully created new system."
+            )
+        return http.HttpResponseBadRequest(
+            "System to create is not valid."
+        )
+
+    @staticmethod
+    def put(request: http.HttpRequest) -> http.HttpResponse:
+        system_data = JSONParser().parse(request)
+        if "id" not in system_data:
+            return http.HttpResponseBadRequest(
+                "Update system request should contain the id field of the system."
+            )
+        try:
+            system = System.objects.get(id=system_data['id'])
+        except System.DoesNotExist:
+            return http.HttpResponseBadRequest(
+                f"System with id '{system_data['id']}' does not exist in database."
+            )
+        system_serializer = SystemSerializer(system, data=system_data)
+        if system_serializer.is_valid():
+            old_name = system.name
+            system_serializer.save()
+            return http.HttpResponse(
+                f"Successfully updated system {old_name}."
+            )
+        return http.HttpResponseBadRequest(
+            f"System {system.name} to update is not valid."
+        )
+
+    @staticmethod
+    def delete(request: http.HttpRequest) -> http.HttpResponse:
+        system_data = JSONParser().parse(request)
+        if "id" not in system_data:
+            return http.HttpResponseBadRequest(
+                "Delete system request should contain the id field of the system."
+            )
+        try:
+            system = System.objects.get(id=system_data['id'])
+        except System.DoesNotExist:
+            return http.HttpResponseBadRequest(
+                f"System with id '{system_data['id']}' does not exist in database."
+            )
+        deleted_system_name = system.name
+        system.delete()
+        return http.HttpResponse(
+            f"Successfully deleted system {deleted_system_name}."
+        )
