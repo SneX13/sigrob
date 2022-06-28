cbdead7b67fb38de95e69649ae4e2349fa980298
Nina Vinding Blindheim <ninamarstrander@gmail.com>
2022-06-29 01:05:37 +0200

    Refactor authorization views

--- backend/app/views/authorization.py
+++ backend/app/views/authorization.py
@@ -1,114 +1,97 @@
-from django import http
 from django.contrib.auth import authenticate, login, logout
-from rest_framework.decorators import api_view
+from django.http import HttpRequest, HttpResponse, HttpResponseBadRequest
 from rest_framework.parsers import JSONParser
 from rest_framework.renderers import JSONRenderer
-from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from ..models import User
 from ..serializers import UserSerializer
 
 
-@api_view(['GET'])
-def get_routes(request):
-    routes = [
-        '/api/token',
-        '/api/token/refresh',
-    ]
-
-    return Response(routes)
-
 class UserData(APIView):
-    @staticmethod
-    def get(request: http.HttpRequest) -> http.HttpResponse:
-        user_data = JSONParser().parse(request)
-        if "email" not in user_data:
-            return http.HttpResponseBadRequest(
-                "User data request should contain an email field."
-            )
-        email = user_data["email"]
-        try:
-            user = User.objects.get(email=email)
-        except User.DoesNotExist:
-            return http.HttpResponseBadRequest(
-                f"User with email '{email}' does not exist in database."
-            )
-        user_serializer = UserSerializer(user, many=False)
-        json_data = JSONRenderer().render(user_serializer.data)
-        return http.HttpResponse(json_data)
+    @classmethod
+    def get(cls, request: HttpRequest) -> HttpResponse:
+        email, = extract_request_fields(request, "email")
+        if email is None:
+            return no_email_field_response(cls.__name__)
+        user = get_user_by_email_field(email)
+        if user is None:
+            return user_does_not_exist_response(email)
+        return user_data_response(user, many=False)
 
-"""
-class Login(APIView):
-    @staticmethod
-    def post(request: http.HttpRequest) -> http.HttpResponse:
-        login_data = JSONParser().parse(request)
-        if "email" not in login_data or "password" not in login_data:
-            return http.HttpResponseBadRequest(
-                "Login request should contain email and password fields."
-            )
-        email, password = login_data["email"], login_data["password"]
-        try:
-            User.objects.get(email=email)
-        except User.DoesNotExist:
-            return http.HttpResponseBadRequest(
-                f"User with email '{email}' does not exist in database."
-            )
-        user = authenticate(request, email=email, password=password)
-        if user is not None:
-            user_serializer = UserSerializer(user, many=False)
-            json_data = JSONRenderer().render(user_serializer.data)
-            return http.HttpResponse(json_data)
-        else:
-            return http.HttpResponse(
-                f"Email and password combination is not correct."
-            ) """
 
 class LoginAttempt(APIView):
-    @staticmethod
-    def post(request: http.HttpRequest) -> http.HttpResponse:
-        login_data = JSONParser().parse(request)
-        if "email" not in login_data or "password" not in login_data:
-            return http.HttpResponseBadRequest(
-                "Login request should contain email and password fields."
-            )
-        email, password = login_data["email"], login_data["password"]
-        try:
-            User.objects.get(email=email)
-        except User.DoesNotExist:
-            return http.HttpResponseBadRequest(
-                f"User with email '{email}' does not exist in database."
-            )
-        user = authenticate(request, email=email, password=password)
-        if user is not None:
-            login(request, user)
-            user = User.objects.filter(email=email)
-            user_serializer = UserSerializer(user, many=True)
-            json_data = JSONRenderer().render(user_serializer.data)
-            return http.HttpResponse(json_data
-                                     )
-        else:
-            return http.HttpResponse(
+    @classmethod
+    def post(cls, request: HttpRequest) -> HttpResponse:
+        email, password = extract_request_fields(request, "email", "password")
+        if email is None:
+            return no_email_field_response(cls.__name__)
+        if password is None:
+            return no_password_field_response(cls.__name__)
+        user = get_user_by_email_field(email)
+        if user is None:
+            return user_does_not_exist_response(email)
+        authentication = authenticate(request, email=email, password=password)
+        if authentication is None:
+            return HttpResponse(
                 f"Email and password combination is not correct."
             )
+        login(request, authentication)
+        return user_data_response([user], many=True)
 
 
 class Logout(APIView):
-    @staticmethod
-    def post(request: http.HttpRequest) -> http.HttpResponse:
-        logout_data = JSONParser().parse(request)
-        if "email" not in logout_data:
-            return http.HttpResponseBadRequest(
-                "Logout request should contain the email field of the user."
-            )
-        email = logout_data["email"]
-        try:
-            User.objects.get(email=email)
-        except User.DoesNotExist:
-            return http.HttpResponseBadRequest(
-                f"User with email '{email}' does not exist in database."
-            )
+    @classmethod
+    def post(cls, request: HttpRequest) -> HttpResponse:
+        email, = extract_request_fields(request, "email")
+        if email is None:
+            return no_email_field_response(cls.__name__)
+        user = get_user_by_email_field(email)
+        if user is None:
+            return user_does_not_exist_response(email)
         logout(request)
-        return http.HttpResponse(
+        return HttpResponse(
             f"Logout successful for user '{email}'."
         )
+
+
+def no_password_field_response(request_type: str) -> HttpResponseBadRequest:
+    return HttpResponseBadRequest(
+        f"{request_type} request should contain the password field for the user."
+    )
+
+
+def user_does_not_exist_response(email: str) -> HttpResponseBadRequest:
+    return HttpResponseBadRequest(
+        f"User with email '{email}' does not exist in database."
+    )
+
+
+def no_email_field_response(request_type: str) -> HttpResponseBadRequest:
+    return HttpResponseBadRequest(
+        f"{request_type} request should contain the email field for the user."
+    )
+
+
+def extract_request_fields(request: HttpRequest, *fields: str) -> list[str | None]:
+    data = JSONParser().parse(request)
+    values = []
+    for field in fields:
+        if field not in data:
+            values.append(None)
+        else:
+            values.append(data[field])
+    return values
+
+
+def get_user_by_email_field(email: str) -> User | None:
+    try:
+        return User.objects.get(email=email)
+    except User.DoesNotExist:
+        return None
+
+
+def user_data_response(user: User | list[User], many: bool) -> HttpResponse:
+    user_serializer = UserSerializer(user, many=many)
+    json_data = JSONRenderer().render(user_serializer.data)
+    return HttpResponse(json_data)
